e816f28081ad7a05e55a536e535d1edbcef4dabf
Louis Hampton <roastveg@hotmail.co.uk>
2015-04-11 18:49:56 +0100

    Added /n newline functionality to tty_printSring

--- src/kern/video/tty.c
+++ src/kern/video/tty.c
@@ -4,19 +4,27 @@ void tty_clearVideoMemory ( ) {
     char* vid = ( char * ) ADDR_TTY_VIDEO_MEMORY ;
     unsigned int i ;
     
-    for ( i = 0 ; i < 80 * 25 ; i++ ) {
+    for ( i = 0 ; i < CONSOLE_WIDTH * CONSOLE_HEIGHT ; i++ ) {
         *( vid + i ) = 0x00 ;
     }
 }
 
 void tty_printString ( int col , char *str ) {
     char* vid = ( char* ) ADDR_TTY_VIDEO_MEMORY ;
+    int curcol = 0;
     
-    while ( *str != 0x00 && *vid < 80*25 ) {
-        *vid = *str ;
-        vid++ ;
-        *vid = col ;
-        vid++ ;
+    while ( *str != 0x00 && *vid < CONSOLE_WIDTH*CONSOLE_HEIGHT ) {
+	if (*str == CHAR_NEW_LINE ) {
+		vid += (CONSOLE_WIDTH-curcol+1)*2;
+		curcol = 0;
+	} else {
+	        *vid = *str ;
+        	vid++ ;
+		*vid = col ;
+	        vid++ ;
+		curcol++ ;
+		curcol %= CONSOLE_WIDTH ;
+	}
         str++ ;
     }
 }
--- src/kern/video/tty.h
+++ src/kern/video/tty.h
@@ -2,6 +2,9 @@
 #define TTY_VIDEO_H_
 
 #define ADDR_TTY_VIDEO_MEMORY 0xB8000
+#define CHAR_NEW_LINE 10
+#define CONSOLE_WIDTH 80
+#define CONSOLE_HEIGHT 25
 
 void tty_clearVideoMemory ( ) ;
 void tty_printString      ( int , char * ) ;
