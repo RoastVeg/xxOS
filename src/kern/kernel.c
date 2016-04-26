007a4a2a4de34fca92329413c2c25335c6458101
roastveg <lh297@le.ac.uk>
2016-04-26 17:16:54 +0100

    Minor alignment change, added gitignore

--- src/kern/kernel.c
+++ src/kern/kernel.c
@@ -2,6 +2,6 @@
 
 void main ( void ) {
     tty_clearVideoMemory ( ) ;
-    tty_printString ( 0x0F , "Hello, Kernel! I'm testing the way newline works by going overline and screwing up the line breaks \n Banana \n Waffles \n Cream" ) ;
+    tty_printString ( 0x0F , "Hello, Kernel! I'm testing the way newline works by going overline and screwing up the line breaks \nBanana \nWaffles \nCream" ) ;
 }
 
